8838e5162c80b2f84a304aef5ad4bb4c93029f9e
SamMoosavi <hi.sammoosavi@gmail.com>
2024-02-29 19:04:17 +0330

    feat(install-home.sh) add home installer script.

---
+++ scripts/install-home.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+declare username="${username:-$USER}"
+declare home="${home:-/home/$username}"
+
+
+if [ ! -d $home/.dwm ];then
+  mkdir $home/.dwm
+fi
+
+declare dwm="${dwm:-$home/.dwm}"
+
+if [ ! -f $dwm/monitors.sh ];then
+  touch $dwm/monitors.sh
+  chmod +x $dwm/monitors.sh
+fi
+
+if [ ! -f $dwm/autostart.sh ];then
+  touch $dwm/autostart.sh
+  chmod +x $dwm/autostart.sh
+fi
