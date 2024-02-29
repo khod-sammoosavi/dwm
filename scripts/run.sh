98725fed8c859a7067039ba921ae7c8122aecf1c
SamMoosavi <hi.sammoosavi@gmail.com>
2024-02-29 19:04:32 +0330

    chore(run.sh) update.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# setup home directory
+sh /usr/share/dwm/scripts/install-home.sh
+
+# setup monitors
+sh /home/$USER/.dwm/monitors.sh &
+
+# setup autostart applications
+sleep 3 && sh /home/$USER/.dwm/autostart.sh &
+
+# running dwm
 while type dwm >> /dev/null; do dwm && continue || break; done
