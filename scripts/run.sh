c9e325f560a9a56548f4be674aae19c28481b95b
SamMoosavi <hi.sammoosavi@gmail.com>
2024-03-01 18:02:37 +0330

    feat(run.sh) update.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+# compositor
+picom &
+
 # setup home directory
 sh /usr/share/dwm/scripts/install-home.sh
-
-# setup keyboard layouts
-sh /home/$USER/.dwm/keylayouts.sh &
 
 # setup monitors
 sh /home/$USER/.dwm/monitors.sh &
 
-# setup autostart applications
-sleep 3 && sh /home/$USER/.dwm/autostart.sh &
+# setup keyboard layouts
+sh /home/$USER/.dwm/keylayouts.sh &
+
+#setup autostart applications
+sleep 4 && sh /home/$USER/.dwm/autostart.sh &
+
+# dynamic change background
+nitrogen --restore &
 
 # running dwm
 while type dwm >> /dev/null; do dwm && continue || break; done
